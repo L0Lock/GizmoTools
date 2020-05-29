83b4a7cf709f7ece662cdec6b0e4438975e62c5c
-L0Lock- <myuline.annatar@gmail.com>
2020-05-29 10:38:26 +0200

    fix format

--- GizmoSize.py
+++ GizmoSize.py
@@ -16,6 +16,14 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+from bpy.types import (
+    AddonPreferences,
+    Operator,
+    Menu,
+)
+from bpy.props import FloatProperty
+import rna_keymap_ui
+import bpy
 bl_info = {
     "name": "Gizmo Size",
     "description": "Allows you to increase or decrease the Gizmo size using keyboard shortcuts.",
@@ -29,83 +37,81 @@ bl_info = {
     "category": "Interface"
 }
 
-import bpy
-import rna_keymap_ui
-from bpy.props import FloatProperty
-from bpy.types import (
-    AddonPreferences,
-    Operator,
-    Menu,
-)
 
 # -----------------------------------------------------------------------------
-#    Operator : Decrease size    
+#    Operator : Decrease size
 # -----------------------------------------------------------------------------
+
 class VIEW3D_OT_decease_gizmo_size(bpy.types.Operator):
     bl_idname = "view3d.decease_gizmo_size"
     bl_label = "Decrease Gizmo Size"
-    
+
     def execute(self, context):
-        
+
         prefs = context.preferences
         addon_prefs = prefs.addons[__name__].preferences
         view = prefs.view
         gs = view.gizmo_size
-        print ("Gizmo size =", gs)
-        print ("Increment =", addon_prefs.inc)
-        
+        print("Gizmo size =", gs)
+        print("Increment =", addon_prefs.inc)
+
         gs = gs - addon_prefs.inc
         view.gizmo_size = gs
-        print ("New Gizmo size =", gs)
+        print("New Gizmo size =", gs)
         return {'FINISHED'}
 
 # -----------------------------------------------------------------------------
-#    Operator : Increase size    
+#    Operator : Increase size
 # -----------------------------------------------------------------------------
+
+
 class VIEW3D_OT_incease_gizmo_size(bpy.types.Operator):
     bl_idname = "view3d.incease_gizmo_size"
     bl_label = "Increase Gizmo Size"
-    
+
     def execute(self, context):
-        
+
         prefs = context.preferences
         addon_prefs = prefs.addons[__name__].preferences
         view = prefs.view
         gs = view.gizmo_size
-        print ("Gizmo size =", gs)
-        print ("Increment =", addon_prefs.inc)
-        
+        print("Gizmo size =", gs)
+        print("Increment =", addon_prefs.inc)
+
         gs = gs + addon_prefs.inc
         view.gizmo_size = gs
-        print ("New Gizmo size =", gs)
+        print("New Gizmo size =", gs)
         return {'FINISHED'}
-    
+
 # -----------------------------------------------------------------------------
-#    Operator : Move Local Y   
-# -----------------------------------------------------------------------------  
+#    Operator : Move Local Y
+# -----------------------------------------------------------------------------
+
 
 class VIEW3D_OT_move_local_y(bpy.types.Operator):
     bl_idname = "view3d.move_local_y"
     bl_label = "Move on the local Y axis"
-    
+
     @classmethod
     def poll(cls, context):
         return context.active_object is not None
-    
+
     def execute(self, context):
         ot = ops.transform.translate
-        ot(value=(0,1,0), orient_type='LOCAL')  
-        # bpy.ops.transform.translate(value=(0,1,0), orient_type='LOCAL')        
+        ot(value=(0, 1, 0), orient_type='LOCAL')
+        # bpy.ops.transform.translate(value=(0,1,0), orient_type='LOCAL')
         return {'FINISHED'}
 
 # -----------------------------------------------------------------------------
-#    Preferences      
+#    Preferences
 # -----------------------------------------------------------------------------
+
+
 class VIEW3D_PT_gizmo_size_preferences(AddonPreferences):
     bl_idname = __name__
 
     # Increment value
-    inc : FloatProperty(
+    inc: FloatProperty(
         name="Increment",
         description="Increment in px.",
         default=20,
@@ -114,7 +120,7 @@ class VIEW3D_PT_gizmo_size_preferences(AddonPreferences):
         step=100,
         precision=0,
         subtype="PIXEL"
-        )
+    )
 
     # Draws addon preferences
     def draw(self, context):
@@ -125,8 +131,10 @@ class VIEW3D_PT_gizmo_size_preferences(AddonPreferences):
         row.prop(self, "inc", toggle=True)
 
 # -----------------------------------------------------------------------------
-#    Gizmo Menu      
+#    Gizmo Menu
 # -----------------------------------------------------------------------------
+
+
 class VIEW3D_MT_gizmo_size_menu(bpy.types.Menu):
     bl_label = "Gizmo"
     bl_idname = "VIEW3D_MT_gizmo_menu"
@@ -136,18 +144,20 @@ class VIEW3D_MT_gizmo_size_menu(bpy.types.Menu):
         layout.operator("view3d.incease_gizmo_size", icon="ZOOM_IN")
         layout.operator("view3d.decease_gizmo_size", icon="ZOOM_OUT")
 
+
 def draw_gizmo_menu(self, context):
     layout = self.layout
     layout.menu(VIEW3D_MT_gizmo_size_menu.bl_idname)
 
+
 # -----------------------------------------------------------------------------
-#    Keymap   
+#    Keymap
 # -----------------------------------------------------------------------------
 addon_keymaps = []
 
 
 # -----------------------------------------------------------------------------
-#    Register   
+#    Register
 # -----------------------------------------------------------------------------
 
 def register():
@@ -158,16 +168,21 @@ def register():
     bpy.utils.register_class(VIEW3D_PT_gizmo_size_preferences)
     bpy.types.VIEW3D_MT_view.append(draw_gizmo_menu)
     bpy.types.IMAGE_MT_view.append(draw_gizmo_menu)
-    
+
     # Keymap reg
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
     if kc:
-        km = kc.keymaps.new(name='Window', region_type='WINDOW', space_type='EMPTY')
-        kmi = km.keymap_items.new("view3d.decease_gizmo_size", type='PAGE_DOWN', value='PRESS')
-        km = kc.keymaps.new(name='Window', region_type='WINDOW', space_type='EMPTY')
-        kmi = km.keymap_items.new("view3d.incease_gizmo_size", type='PAGE_UP', value='PRESS')
+        km = kc.keymaps.new(
+            name='Window', region_type='WINDOW', space_type='EMPTY')
+        kmi = km.keymap_items.new(
+            "view3d.decease_gizmo_size", type='PAGE_DOWN', value='PRESS')
+        km = kc.keymaps.new(
+            name='Window', region_type='WINDOW', space_type='EMPTY')
+        kmi = km.keymap_items.new(
+            "view3d.incease_gizmo_size", type='PAGE_UP', value='PRESS')
         addon_keymaps.append((km, kmi))
+
 
 def unregister():
     bpy.utils.unregister_class(VIEW3D_PT_gizmo_size_preferences)
@@ -179,6 +194,6 @@ def unregister():
     bpy.types.IMAGE_MT_view.remove(draw_gizmo_menu)
 
     # Keymap unreg
-    for km,kmi in addon_keymaps:
+    for km, kmi in addon_keymaps:
         km.keymap_items.remove(kmi)
     addon_keymaps.clear()
